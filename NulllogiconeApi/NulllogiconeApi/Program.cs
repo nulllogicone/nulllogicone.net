1d8069d9e30b682f7dd2abb24eea1a718e2f0c0b
Frederic Luchting <github@luchting.de>
2025-10-26 20:25:12 +0100

    Add more bruno requests

--- NulllogiconeApi/NulllogiconeApi/Program.cs
+++ NulllogiconeApi/NulllogiconeApi/Program.cs
@@ -39,24 +39,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 // Nulllogicone API Info
 app.MapGet("/about", () => new
@@ -76,6 +59,7 @@ app.MapGet("/about", () => new
 .WithTags("Info");
 
 // Map organized endpoints
+// This levels up minimal APIs to a more structured format
 app.MapStammEndpoints();
 app.MapPostItEndpoints();
 app.MapTopLabEndpoints();
@@ -86,7 +70,3 @@ app.Run();
 
 
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
