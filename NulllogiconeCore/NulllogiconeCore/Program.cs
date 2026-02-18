398fef39ff8608ab80f98ce2560c3acf86f7a5e9
frederic luchting <frederic@luchting.de>
2026-02-18 09:17:09 +0100

    Update Index page and allow swagger everywhere

--- NulllogiconeCore/NulllogiconeCore/Program.cs
+++ NulllogiconeCore/NulllogiconeCore/Program.cs
@@ -50,21 +50,15 @@ builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsigh
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    //app.UseOpenApi();
-    //app.UseSwaggerUi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "v1");
-    });
 
-    // Ensure database is created
-    using var scope = app.Services.CreateScope();
-    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    //context.Database.EnsureCreated();
-}
+app.MapOpenApi();
+//app.UseOpenApi();
+//app.UseSwaggerUi();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
+
 
 app.UseCors();
 app.UseHttpsRedirection();
